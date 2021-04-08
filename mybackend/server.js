7b6fa402b4f5a2f063d1746ee87434a421b77981
seekerpig <laserklet@gmail.com>
2021-04-08 22:35:08 +0800

    successfully configured backend server to connect to weather and front end server to talk to backend and fetch api

--- mybackend/server.js
+++ mybackend/server.js
@@ -12,9 +12,9 @@ app.get('/getWeathersingapore', (req, res) => {
         "http://api.openweathermap.org/data/2.5/weather?q=singapore&appid=92ea3bf577512e6361472a34784831c7",
         function(error, response, body){
             if (!error && response.statusCode === 200){
-                var parsedBody = JSON.parse(body);
+                //var parsedBody = JSON.parse(body);
                 //res.send(parsedBody); // print the obj after JSON.parse
-                res.send(`The city is ${parsedBody.name}, temperature is ${parsedBody.main.temp}`);
+                res.send(body);
             }
         }
     );
--- myfrontend/src/App.js
+++ myfrontend/src/App.js
@@ -5,6 +5,7 @@ import {PhotoCamera} from '@material-ui/icons';
 
 import {makeStyles} from '@material-ui/core/styles'
 
+
 const useStyles = makeStyles((props) => ({
     container:{
         backgroundColor: props.palette.background.paper,
@@ -40,12 +41,30 @@ const useStyles = makeStyles((props) => ({
     },
 }));
 
-const cards = [1,2,3,4,5,6];
+const cards = [1,2,3,4,5];
 
 
 function App() {
 
     const classes = useStyles();
+    const [weather, setWeather] = React.useState({'name':'yet to be retrieved', main:{temp:'undefined'}});
+    
+    React.useEffect(() =>{
+        document.title=`Current weather is : ${weather.name}.`
+    });
+    
+    React.useEffect(() => {
+        fetch('/getWeathersingapore')
+            .then(res => res.json())
+            .then((res)=> {setWeather(res);});
+
+            
+        
+            /*.then(res => res.json())
+            .then((res) => console.log("weather retrieved",res));*/
+            
+    });
+
     return(
         <React.Fragment>
             <CssBaseline/>
@@ -76,12 +95,29 @@ function App() {
                 </div>
                 <Container className={classes.cardGrid} maxWidth="md">
                     <Grid container spacing={4}>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <Card className={classes.card}>
+                                <CardMedia className = {classes.cardMedia} image="https://source.unsplash.com/random" title="Image Title"/>
+                                <CardContent className = {classes.cardContent}>
+                                    <Typography gutterBottom variant="h5">
+                                        {weather.name}
+                                    </Typography>
+                                    <Typography>
+                                        {`Current weather in ${weather.name} is ${Math.round((weather.main.temp - 273.1) * 10) / 10}`}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">View</Button>
+                                    <Button size="small" color="primary" onClick={() => setWeather({name:"Singa",main:{temp:'undefined'}})}>Edit</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
                         {cards.map((card)=>(
                             <Grid item key ={card} xs={12} sm={6} md={4}>
                                 <Card className={classes.card}>
                                     <CardMedia className = {classes.cardMedia} image="https://source.unsplash.com/random" title="Image Title"/>
                                     <CardContent className = {classes.cardContent}>
-                                        <Typography gutterbottom variant="h5">
+                                        <Typography gutterBottom variant="h5">
                                             Heading
                                         </Typography>
                                         <Typography>
